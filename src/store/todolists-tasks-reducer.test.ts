7edfbeb7f3d36f4edfb52e42f9e4148359d29fac
Kseniya Melnik <kseniyamelnnik@gmail.com>
2022-01-21 17:38:18 +0300

    fix: change test for todolist and task reducers

--- src/store/todolists-tasks-reducer.test.ts
+++ src/store/todolists-tasks-reducer.test.ts
@@ -1,12 +1,14 @@
-import {TasksStateType, TodolistType} from "../App";
-import {AddTodoListAC, todoListsReducer} from "./todoLists-reducer";
+import {TasksStateType} from "../App/AppWidtxRedux";
+import {AddTodoListAC, TodolistDomainType, todoListsReducer} from "./todoLists-reducer";
 import {tasksReducer} from "./tasks-reducer";
 
 test('ids should be equals', () => {
     const startTasksState: TasksStateType = {};
-    const startTodolistsState: Array<TodolistType> = [];
+    const startTodolistsState: Array<TodolistDomainType> = [];
 
-    const action = AddTodoListAC("new todolist");
+    const newTodolist =  {id: "newId", title: "What to learn", addedDate: "01.01.2021", order: 1, filter: "all", entityStatus: "idle"}
+
+        const action = AddTodoListAC(newTodolist);
 
     const endTasksState = tasksReducer(startTasksState, action)
     const endTodolistsState = todoListsReducer(startTodolistsState, action)
@@ -15,6 +17,6 @@ test('ids should be equals', () => {
     const idFromTasks = keys[0];
     const idFromTodolists = endTodolistsState[0].id;
 
-    expect(idFromTasks).toBe(action.todolistId);
-    expect(idFromTodolists).toBe(action.todolistId);
+    expect(idFromTasks).toBe(action.todolist.id);
+    expect(idFromTodolists).toBe(action.todolist.id);
 });
